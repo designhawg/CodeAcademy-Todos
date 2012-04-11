630ca2a46e8cbe572115319100f508bfc8644584
Ben Reinhart <breinhart@groupon.com>
2012-04-11 15:34:23 -0500

    Finished 'final' version

--- public/javascripts/todos_final.js
+++ public/javascripts/todos_final.js
@@ -1,27 +1,59 @@
 var Todo = {
   init: function() {
-    this.clickListener();
+    this.setupAddListener();
+    this.setupDeleteListener();
   },
   
-  clickListener: function() {
+  setupAddListener: function() {
     var that = this;
     $('#submit').bind('click', function() {
       var value = that.getValue();
-      that.addItem(value);
+      that.addTodo(value);
+      that.clearValue();
+      that.insertCount();
     });
   },
-  
-  addItem: function(value) {
-    var template = '<li>' + value + '</li>';
-    $('#todos').append(template);
+
+  setupDeleteListener: function() {
+    var that = this;
+    $('#todos').on('click', '.delete', function() {
+      var li = $(this).parent('li');
+      that.deleteTodo(li);
+      that.insertCount();
+    });
+  },
+
+  constructDeleteButton: function() {
+    return ' <span class="delete">X</span>';
   },
   
+  addTodo: function(value) {
+    var deleteButton = this.constructDeleteButton();
+    var template = '<li>' + value + deleteButton + '</li>';
+    $('#todos').append(template);
+  },
+
+  deleteTodo: function(todo) {
+    todo.remove();
+  },
+  
+  insertCount: function() {
+    $('#count').html(this.getCount());
+  },
+
   getValue: function() {
-    return $('#todo').val();            
+    return $('#todo').val();          
+  },
+
+  clearValue: function() {
+    $('#todo').val('');
+  },
+
+  getCount: function() {
+    return $('#todos').children('li').length;
   }
 };
 
-
-$(document).ready(function() {
+$(function() {
   Todo.init();
 });
